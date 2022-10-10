5c7d1a0d4cfee26c01d0ba70cf090d6d304ccd50
subru-37 <easwaransubramaniOO7@gmail.com>
2022-10-11 00:54:01 +0530

    first commit

--- src/components/Main/Overlay/Overlay.js
+++
@@ -1,7 +0,0 @@
-/** @jsx jsx */
-import { jsx, css } from "@emotion/core";
-import React from "react";
-import './Overlay.css'
-
-const Overlay = () => <div  className="overlay"></div>;
-export default Overlay;
