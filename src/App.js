ba5fd963f54336112412cd72107b586bb6481305
Karthik G Kumar <karthikgkumar2002@gmail.com>
2022-10-12 01:15:50 +0530

    contact page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import { Navbar, Footer, Loader } from "./components";
+import Contact from './components/Contact/Contact';
 import { HomePage } from './pages'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             
             <Route path="*" element={<Navigate replace to="/" />} />
           </Routes>
+          <Contact />
           <Footer />
         </BrowserRouter>
       </Suspense>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,70 +1,53 @@
-import React, { useState } from "react"
-import Drawer from "@mui/material/Drawer"
-import { IoMenu, IoClose } from "react-icons/io5"
-import { NavHashLink as NavLink } from "react-router-hash-link"
-import { useScrollPosition } from "../../hooks/useScrollPosition"
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { NavHashLink as NavLink } from "react-router-hash-link";
+import { useScrollPosition } from "../../hooks/useScrollPosition";
 
-import "./Navbar.css"
-import logo from '../../assets/excel_logo_png.png';
+import "./Navbar.css";
+import logo from "../../assets/excel_logo_png.png";
 
 function Navbar() {
-  const [drawer, setDrawer] = useState(false)
+  const [drawer, setDrawer] = useState(false);
 
   const handleDrawerOpen = () => {
-    setDrawer(true)
-  }
+    setDrawer(true);
+  };
 
   const handleDrawerClose = () => {
-    setDrawer(false)
-  }
+    setDrawer(false);
+  };
 
-  const scrollPosition = useScrollPosition()
+  const scrollPosition = useScrollPosition();
 
   const navStyle = {
-    background: scrollPosition > 200 ? "transparent" : "transparent",//"#04005e"
+    background: scrollPosition > 200 ? "transparent" : "transparent", //"#04005e"
     boxShadow:
       scrollPosition > 200 ? "0px 2px 9px rgba(0, 0, 0, 0.11)" : "none",
-  }
+  };
 
   return (
     <div className="navbar" style={navStyle}>
       <div className="navbar__main container">
         <div className="nav_logo">
-        <NavLink
-            to="/#"
-          >
-          <img src={logo} alt="" />
+          <NavLink to="/#">
+            <img src={logo} alt="" />
           </NavLink>
         </div>
         <div className="nav_contents">
-          <NavLink
-            to="/#home"
-            className="nav__link"
-          >
+          <NavLink to="/#home" className="nav__link">
             HOME
           </NavLink>
-          <NavLink
-            to="/#about"
-            className="nav__link"
-          >
+          <NavLink to="/#about" className="nav__link">
             ABOUT
           </NavLink>
-          <NavLink
-            to="/#rewards"
-            className="nav__link"
-          >
+          <NavLink to="/#rewards" className="nav__link">
             REWARDS
           </NavLink>
-          <NavLink
-            to="/#faq"
-            className="nav__link"
-          >
+          <NavLink to="/#faq" className="nav__link">
             FAQ
           </NavLink>
-          <NavLink
-            to="/#contact"
-            className="nav__link"
-          >
+          <NavLink to="/#contact" className="nav__link">
             CONTACT
           </NavLink>
         </div>
@@ -128,7 +111,7 @@ function Navbar() {
         </div>
       </Drawer>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
