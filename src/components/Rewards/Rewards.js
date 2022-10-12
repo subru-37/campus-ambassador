44ee25d8aac6514feb42b68878df485af467f393
hacksh4w <91671136+hacksh4w@users.noreply.github.com>
2022-10-13 02:08:54 +0530

    Update Rewards.js

--- src/components/Rewards/Rewards.js
+++ src/components/Rewards/Rewards.js
@@ -5,11 +5,7 @@ import { SvgIcon,createSvgIcon } from '@mui/material';
 import RewardItem from './RewardItem';
 
 export default function Rewards() {
-  { /*const data = [
-    {no: '1',name:'CERTIFICATES'},
-    {no: '2',name:'SWAGS'},
-    {no: '3',name:'PRIDE'}
-  ] */}
+
   const Ray1Icon = createSvgIcon(
     <path d="M4,9C5.31,9 6.42,9.83 6.83,11H22V13H6.83C6.42,14.17 5.31,15 4,15A3,3 0 0,1 1,12A3,3 0 0,1 4,9Z" />,
     'Ray1',
@@ -21,20 +17,19 @@ export default function Rewards() {
   
   return (
     <div className="rewards">
-      <div className="Headline"> 
+      <div className="headline"> 
         <div className="left-rays">
           <Ray1Icon className= "svgrays"/>
           <Ray1Icon className= "svgrays"/>
         </div>
         <h1 className="rewards-text" sx={{ 
           textAlign: 'center'}} >PERKS AND REWARDS </h1> 
-        <div className="right-rays">
+        <div className="right-rays" >
           <Ray2Icon className= "svgrays" /> 
           <Ray2Icon className= "svgrays" />
-      
         </div>
       </div>    
-      <div className='Items'>
+      <div className='items'>
         <RewardItem />
         <RewardItem />
         <RewardItem />
