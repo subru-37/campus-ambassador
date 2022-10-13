0fce6f13dd09b1c2774dd1971066eada60cc4abb
Anjana Sudevan <anjanasudevan38@gmail.com>
2022-10-13 18:51:17 +0530

    slider

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Slider from "react-slick";
 import excel_img from './../../assets/excel_img.svg'
 import {useScreenWidth} from './../../../src/hooks/useScreenWidth'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import './About.css'
 
